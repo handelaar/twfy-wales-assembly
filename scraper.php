0405fafae103984beb37d9cf48dc744047641b62
John Handelaar <john@handelaar.org>
2014-04-01 16:06:47 +0100

    First morph test

--- scraper.php
+++ scraper.php
@@ -27,7 +27,7 @@ foreach(range(1,3) as $month) {
 		if(trim($target->plaintext) === "Written Assembly Questions and Answers") {
 			$url = "http://assemblywales.org/" . str_replace("&amp;","&",$target->href);
 			#$existingRecords = scraperwiki::select("* from data where `url`='" . $url . "'");
-			
+			echo "$url\n"
 			#if (sizeof($existingRecords) == 0) {
 				scraperwiki::save(array('url'), array($url));
 			#}
